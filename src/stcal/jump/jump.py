e3a6e386def88028857d6242b2649989d9aa6bce
Mike Regan <mregan@stsci.edu>
2023-07-07 10:44:34 -0400

    Update jump.py

--- src/stcal/jump/jump.py
+++ src/stcal/jump/jump.py
@@ -718,7 +718,7 @@ def find_faint_extended(indata, gdq, readnoise_2d, nframes, minimum_sigclip_grou
         # calculate sigma for each pixel
         if nints <= minimum_sigclip_groups:
 #            median_diffs = np.nanmedian(first_diffs_masked[intg], axis=0)
-            median_diffs = np.nanpercentile(first_diffs_masked[intg], 25, axis=0,
+            median_diffs = np.nanpercentile(first_diffs[intg], 25, axis=0,
                                             method='median_unbiased')
             sigma = np.sqrt(np.abs(median_diffs) + read_noise_2 / nframes)
             # The difference from the median difference for each group
