645be370fe262d4e225d97138fcdd7bf67d765e5
Mike Regan <mregan@stsci.edu>
2024-11-13 14:16:11 -0500

    Update jump.py

--- src/stcal/jump/jump.py
+++ src/stcal/jump/jump.py
@@ -1177,7 +1177,7 @@ def flag_previous_saturation(gdq, start_time, detector_name):
     print(today_search)
     today_files = glob(today_search + "*")
     print(today_files)
-    yesterday_search = str(round(float(start_time) - 1)) + "*" + detector_name
+    yesterday_search = str(round(float(start_time) - 1)) + "*" + detector_name + "*"
     print(yesterday_search)
     yesterday_files = glob(yesterday_search + "*")
     print(yesterday_files)
